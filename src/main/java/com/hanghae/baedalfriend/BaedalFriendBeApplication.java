13ac0411047dcf19655eea86ddda8ba981ec476c
BAEKDOOSAN <doosan0425@gmail.com>
2022-11-08 01:08:42 +0900

    main

---
+++ src/main/java/com/hanghae/baedalfriend/BaedalFriendBeApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.baedalfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedalFriendBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaedalFriendBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/baedalfriend/BaedalFriendBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.baedalfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedalFriendBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
