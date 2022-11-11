72d1c27d9f0dc76a131ff9460139b4fce97ca909
kaifazhehj99 <kaifazhehj99@gmail.com>
2022-11-11 17:29:50 +0900

    Fix: ChatRoomMemberRepository deleteByMemberId > deleteByMember findAllByChatRoomId findAllByChatRoom

--- src/main/java/com/hanghae/baedalfriend/chat/repository/ChatRoomMemberRepository.java
+++ src/main/java/com/hanghae/baedalfriend/chat/repository/ChatRoomMemberRepository.java
@@ -3,15 +3,17 @@ package com.hanghae.baedalfriend.chat.repository;
 
 import com.hanghae.baedalfriend.chat.entity.ChatRoom;
 import com.hanghae.baedalfriend.chat.entity.ChatRoomMember;
+import com.hanghae.baedalfriend.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
-    ChatRoomMember deleteByMemberId(Long id);
+    ChatRoomMember deleteByMember (Member member);
 
-    List<ChatRoomMember> findByChatRoomId(Long id);
+    List<ChatRoomMember> findAllByChatRoom(ChatRoom chatRoom);
 
-    ChatRoomMember findByChatRoomIdAndMemberId(Long id, Long member_id);
+
 
 }
