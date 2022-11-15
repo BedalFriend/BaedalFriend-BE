f46f223a9fe210f3cf6d861260f6f558d2c1d15f
ssoyeon59 <nebula059@naver.com>
2022-11-16 01:18:12 +0900

    Fix: Change S3Service

--- src/main/java/com/hanghae/baedalfriend/service/S3Service.java
+++ src/main/java/com/hanghae/baedalfriend/service/S3Service.java
@@ -1,22 +1,21 @@
 package com.hanghae.baedalfriend.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.*;
 import com.hanghae.baedalfriend.dto.PhotoDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,28 +28,32 @@ public class S3Service {
 
     private final AmazonS3 amazonS3;
 
-    public List<PhotoDto> uploadImage(List<MultipartFile> multipartFile) {
+    @Transactional
+    public List<PhotoDto> uploadImage(MultipartFile multipartFile) {
+        if (multipartFile == null) {
+            return Collections.emptyList();
+        }
         List<PhotoDto> photoDtos = new ArrayList<>();
-        multipartFile.forEach(file -> {
-            String fileName = createFileName(file.getOriginalFilename());
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(file.getSize());
-            objectMetadata.setContentType(file.getContentType());
 
-            try(InputStream inputStream = file.getInputStream()) {
-                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
-                        .withCannedAcl(CannedAccessControlList.PublicRead));
-            } catch(IOException e) {
-                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다.");
-            }
+        String fileName = createFileName(multipartFile.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(multipartFile.getSize());
+        objectMetadata.setContentType(multipartFile.getContentType());
 
-            log.info("imageUrl : " + fileName);
-            PhotoDto photoDto = PhotoDto.builder()
-                    .key(fileName)
-                    .path(amazonS3.getUrl(bucket, fileName).toString())
-                    .build();
-            photoDtos.add(photoDto);
-        });
+        try(InputStream inputStream = multipartFile.getInputStream()) {
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch(IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다.");
+        }
+
+        log.info("imageUrl : " + fileName);
+        PhotoDto photoDto = PhotoDto.builder()
+                .key(fileName)
+                .path(amazonS3.getUrl(bucket, fileName).toString())
+                .build();
+        photoDtos.add(photoDto);
+
 
         return photoDtos;
     }
@@ -70,38 +73,11 @@ public class S3Service {
         }
     }
 
-    public List<PhotoDto> updateImage(List<MultipartFile> multipartFile, String currentFilePath) {
-        List<PhotoDto> photoDtos = new ArrayList<>();
-        multipartFile.forEach(file -> {
-            String fileName = createFileName(file.getOriginalFilename());
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(file.getSize());
-            objectMetadata.setContentType(file.getContentType());
-
-            //기존 이미지 삭제
-            amazonS3.deleteObject(new DeleteObjectRequest(bucket, currentFilePath));
-
-            //수정 이미지 업로드
-            try(InputStream inputStream = file.getInputStream()) {
-                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
-                        .withCannedAcl(CannedAccessControlList.PublicRead));
-            } catch(IOException e) {
-                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다.");
-            }
-            log.info("imageUrl : " + fileName);
-            PhotoDto photoDto = PhotoDto.builder()
-                    .key(fileName)
-                    .path(amazonS3.getUrl(bucket, fileName).toString())
-                    .build();
-            photoDtos.add(photoDto);
-        });
-        return photoDtos;
-    }
-
     public void deleteImage(String imageUrl) {
-        System.out.println("fileName : " + imageUrl);
         String fileName = imageUrl.substring(51); //누구의 S3가 됐든 앞의 URL부분을 적어야 한다..
+        System.out.println("fileName : " + fileName);
         amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
         System.out.println("삭제완료");
+
     }
 }
