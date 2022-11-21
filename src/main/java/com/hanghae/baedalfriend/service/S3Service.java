238d1f70477a53285cc14b2f66f63ebbeec3b3ec
BAEKDOOSAN <doosan0425@gmail.com>
2022-11-21 23:33:42 +0900

    Fix search 지역검색기능 추가, 최대수용인원 컬럼 추가

--- src/main/java/com/hanghae/baedalfriend/service/S3Service.java
+++ src/main/java/com/hanghae/baedalfriend/service/S3Service.java
@@ -1,83 +1,83 @@
-//package com.hanghae.baedalfriend.service;
-//
-//import com.amazonaws.services.s3.AmazonS3;
-//import com.amazonaws.services.s3.model.*;
-//import com.hanghae.baedalfriend.dto.PhotoDto;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.log4j.Log4j2;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.web.multipart.MultipartFile;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Service
-//@Log4j2
-//@RequiredArgsConstructor
-//public class S3Service {
-//    @Value("${cloud.aws.s3.bucket}")
-//    private String bucket;
-//
-//    private final AmazonS3 amazonS3;
-//
-//    @Transactional
-//    public List<PhotoDto> uploadImage(MultipartFile multipartFile) {
-//        if (multipartFile == null) {
-//            return Collections.emptyList();
-//        }
-//        List<PhotoDto> photoDtos = new ArrayList<>();
-//
-//        String fileName = createFileName(multipartFile.getOriginalFilename());
-//        ObjectMetadata objectMetadata = new ObjectMetadata();
-//        objectMetadata.setContentLength(multipartFile.getSize());
-//        objectMetadata.setContentType(multipartFile.getContentType());
-//
-//        try(InputStream inputStream = multipartFile.getInputStream()) {
-//            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
-//                    .withCannedAcl(CannedAccessControlList.PublicRead));
-//        } catch(IOException e) {
-//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다.");
-//        }
-//
-//        log.info("imageUrl : " + fileName);
-//        PhotoDto photoDto = PhotoDto.builder()
-//                .key(fileName)
-//                .path(amazonS3.getUrl(bucket, fileName).toString())
-//                .build();
-//        photoDtos.add(photoDto);
-//
-//
-//        return photoDtos;
-//    }
-//    private String createFileName(String fileName) { // 먼저 파일 업로드 시, 파일명을 난수화하기 위해 random으로 돌립니다.
-//        // 파일 이름의 길이가 길면 100자로 자르기 (디비의 varchar(255) 제한에 걸리지 않으려고)
-//        if (fileName.length()>100){
-//            fileName = fileName.substring(0,100);
-//        }
-//        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
-//    }
-//
-//    private String getFileExtension(String fileName) { // file 형식이 잘못된 경우를 확인하기 위해 만들어진 로직이며, 파일 타입과 상관없이 업로드할 수 있게 하기 위해 .의 존재 유무만 판단하였습니다.
-//        try {
-//            return fileName.substring(fileName.lastIndexOf("."));
-//        } catch (StringIndexOutOfBoundsException e) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일(" + fileName + ") 입니다.");
-//        }
-//    }
-//
-//    public void deleteImage(String imageUrl) {
-//        String fileName = imageUrl.substring(51); //누구의 S3가 됐든 앞의 URL부분을 적어야 한다..
-//        System.out.println("fileName : " + fileName);
-//        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
-//        System.out.println("삭제완료");
-//
-//    }
-//}
+package com.hanghae.baedalfriend.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import com.hanghae.baedalfriend.dto.PhotoDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class S3Service {
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
+    @Transactional
+    public List<PhotoDto> uploadImage(MultipartFile multipartFile) {
+        if (multipartFile == null) {
+            return Collections.emptyList();
+        }
+        List<PhotoDto> photoDtos = new ArrayList<>();
+
+        String fileName = createFileName(multipartFile.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(multipartFile.getSize());
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        try(InputStream inputStream = multipartFile.getInputStream()) {
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch(IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다.");
+        }
+
+        log.info("imageUrl : " + fileName);
+        PhotoDto photoDto = PhotoDto.builder()
+                .key(fileName)
+                .path(amazonS3.getUrl(bucket, fileName).toString())
+                .build();
+        photoDtos.add(photoDto);
+
+
+        return photoDtos;
+    }
+    private String createFileName(String fileName) { // 먼저 파일 업로드 시, 파일명을 난수화하기 위해 random으로 돌립니다.
+        // 파일 이름의 길이가 길면 100자로 자르기 (디비의 varchar(255) 제한에 걸리지 않으려고)
+        if (fileName.length()>100){
+            fileName = fileName.substring(0,100);
+        }
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    private String getFileExtension(String fileName) { // file 형식이 잘못된 경우를 확인하기 위해 만들어진 로직이며, 파일 타입과 상관없이 업로드할 수 있게 하기 위해 .의 존재 유무만 판단하였습니다.
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일(" + fileName + ") 입니다.");
+        }
+    }
+
+    public void deleteImage(String imageUrl) {
+        String fileName = imageUrl.substring(51); //누구의 S3가 됐든 앞의 URL부분을 적어야 한다..
+        System.out.println("fileName : " + fileName);
+        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
+        System.out.println("삭제완료");
+
+    }
+}
