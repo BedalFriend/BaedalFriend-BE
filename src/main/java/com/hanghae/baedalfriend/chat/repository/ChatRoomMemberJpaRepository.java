fa116097d3548f0133672e2d66e581f8ac3820fb
kaifazhehj99 <kaifazhehj99@gmail.com>
2022-12-07 08:15:33 +0900

    style: 불필한 요소들 정리

--- src/main/java/com/hanghae/baedalfriend/chat/repository/ChatRoomMemberJpaRepository.java
+++ src/main/java/com/hanghae/baedalfriend/chat/repository/ChatRoomMemberJpaRepository.java
@@ -3,7 +3,6 @@ package com.hanghae.baedalfriend.chat.repository;
 import com.hanghae.baedalfriend.chat.entity.ChatRoom;
 import com.hanghae.baedalfriend.chat.entity.ChatRoomMember;
 import com.hanghae.baedalfriend.domain.Member;
-import com.hanghae.baedalfriend.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
