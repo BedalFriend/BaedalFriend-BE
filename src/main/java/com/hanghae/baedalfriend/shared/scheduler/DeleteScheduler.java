de1405feb8f00f61199105c52f268a996335120c
kaifazhehj99 <kaifazhehj99@gmail.com>
2022-12-08 00:02:30 +0900

    fix: 인자보다 미래일 때 true가 리턴

--- src/main/java/com/hanghae/baedalfriend/shared/scheduler/DeleteScheduler.java
+++ src/main/java/com/hanghae/baedalfriend/shared/scheduler/DeleteScheduler.java
@@ -4,20 +4,24 @@ package com.hanghae.baedalfriend.shared.scheduler;
 import com.hanghae.baedalfriend.domain.Post;
 import com.hanghae.baedalfriend.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DeleteScheduler {
 
 
     private final PostRepository postRepository;
     private final DeleteService deleteService;
-    LocalDateTime now = LocalDateTime.now();
+   
 
 
     @Scheduled(cron = "0 0/1 * * * ?  ") //1분마다 실행
@@ -38,11 +42,15 @@ public class DeleteScheduler {
     }
 
 
+
     public void softDelete() {
+
+        LocalDateTime now = LocalDateTime.now();
         List<Post> postList = postRepository.findAll();
         for (Post post : postList) {
-            //인자보다 과거일 떄 true return
-            if (now.isBefore(post.getLimitTime())) {
+            //인자보다 미래일 때 true가 리턴
+            if (now.isAfter(post.getLimitTime())) {
+
                 deleteService.updateIsDone(post);
             }
         }
