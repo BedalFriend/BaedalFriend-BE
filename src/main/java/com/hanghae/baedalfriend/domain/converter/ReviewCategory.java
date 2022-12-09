1e46357d3a0743f1818fa0ea1b2ee449dfdfa32a
kaifazhehj99 <kaifazhehj99@gmail.com>
2022-12-09 20:08:31 +0900

    Feat: 리뷰창view Api 추가 리뷰작성자제외 해당게시글에 참여했던 채팅방멤버리스트 보내줍니다. querydsl 설정추가

--- src/main/java/com/hanghae/baedalfriend/domain/converter/ReviewCategory.java
+++ src/main/java/com/hanghae/baedalfriend/domain/converter/ReviewCategory.java
@@ -10,9 +10,13 @@ public enum ReviewCategory {
     GOOD1("시간 약속을 잘 지켜요"),
     GOOD2("친절해요"),
     GOOD3("답장이 빨라요"),
-    GOOD4("마무리가 깔끔해요"),
+    GOOD4("거래매너가 좋아요"),
     BAD1("시간 약속을 안 지켜요"),
-    BAD2("매너가 부족해요");
+    BAD2("채팅 매너가 부족해요"),
+    BAD3("연락이 잘 안돼요"),
+    BAD4("거래 매너가 부족해요");
+
+
 
 
     private String value;
--- src/main/java/com/hanghae/baedalfriend/repository/ReviewRepository.java
+++ src/main/java/com/hanghae/baedalfriend/repository/ReviewRepository.java
@@ -5,4 +5,5 @@ import com.hanghae.baedalfriend.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository  extends JpaRepository<Review,Long> {
+
 }
